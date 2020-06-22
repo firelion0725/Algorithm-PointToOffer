76500d816d3b60248e1c269643ab3b713036cc3e
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-06-22 17:13:18 +0800

    1.增加注释

--- src/com/leo/algorithm/work/ReversePrint.java
+++ src/com/leo/algorithm/work/ReversePrint.java
@@ -4,6 +4,10 @@ import com.leo.algorithm.model.ListNode;
 
 import java.util.Stack;
 
+/**
+ * 剑指 Offer 06	 从尾到头打印链表
+ * https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+ */
 public class ReversePrint {
 
 
