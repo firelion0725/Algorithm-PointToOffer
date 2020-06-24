ad9d400db89e3e9704269577bea8fc3eda28617e
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-06-24 11:24:31 +0800

    1.剑指 Offer 54 二叉搜索树的第k大节点

---
+++ src/com/leo/algorithm/work/KthLargest.java
@@ -0,0 +1,31 @@
+package com.leo.algorithm.work;
+
+import com.leo.algorithm.model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KthLargest {
+
+    public int kthLargest(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        helper(root, list);
+        if (k > list.size()) {
+            return 0;
+        }
+        int index = list.size() - k;
+        return list.get(index);
+    }
+
+    private void helper(TreeNode node, List<Integer> list) {
+        if (node.left != null) {
+            helper(node.left, list);
+        }
+
+        list.add(node.val);
+
+        if (node.right != null) {
+            helper(node.right, list);
+        }
+    }
+}
