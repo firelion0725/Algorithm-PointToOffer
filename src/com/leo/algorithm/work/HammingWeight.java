531f14df92bcb641e9c492df790cd538125aa4c3
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-07-07 11:05:15 +0800

    剑指 Offer 15. 二进制中1的个数

---
+++ src/com/leo/algorithm/work/HammingWeight.java
@@ -0,0 +1,13 @@
+package com.leo.algorithm.work;
+
+public class HammingWeight {
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        while(n!=0){
+            count++;
+            n = n&(n-1);
+        }
+        return count;
+    }
+}
