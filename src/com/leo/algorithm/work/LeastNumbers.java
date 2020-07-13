49642fe6b9e9a0a25d2224a2eca343248eade493
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-07-13 11:24:24 +0800

    剑指 Offer 40. 最小的k个数

---
+++ src/com/leo/algorithm/work/LeastNumbers.java
@@ -0,0 +1,20 @@
+package com.leo.algorithm.work;
+
+import java.util.Arrays;
+
+public class LeastNumbers {
+
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (arr.length <= k) {
+            return arr;
+        }
+
+        int[] res = new int[k];
+
+        Arrays.sort(arr);
+        for (int i = 0; i <= k; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+}
